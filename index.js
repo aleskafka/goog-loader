86c0983efc7f5438d4973405bb221af509b78694
Ales Kafka <ales.kafka@gmail.com>
2016-03-28 22:01:46 +0200

    Add support for recompile when namespace is missing

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var dependencies = require('./lib/dependencies');
 var loaderDevelopment = require('./lib/loader.development');
 var loaderProduction = require('./lib/loader.production');
 
+var cache = {};
 
 module.exports = function(source, map) {
 	this.cacheable(true);
@@ -13,25 +14,33 @@ module.exports = function(source, map) {
 		this.emitWarning('goog-loader is applied twice.\nPossible problem is with providing variable, fix with !!goog! as loader.');
 	}
 
+	if (cache[this.resourcePath] && cache[this.resourcePath].source===source) {
+		return this.callback(null, cache[this.resourcePath].output, map);
+	}
+
 	var that = this;
 	var resourcePath = this.resourcePath;
 	var callback = this.async();
 	var production = process.env.NODE_ENV==='production';
 	var loader = production ? loaderProduction.bind(this) : loaderDevelopment.bind(this);
 
-	dependencies.changed(resourcePath, function() {
+	options.js.forEach(this.addContextDependency.bind(this));
+
+	dependencies.changed(resourcePath, cache[resourcePath]&&cache[resourcePath].error, function() {
 		if (dependencies.isReachable(resourcePath)) {
 			var request = "!!goog/"
 				+ (production ? "build" : "base")
 				+ "!"
 				+ (production ? options.entry : options._basePath);
 
-			source = "require(" + JSON.stringify(request) + ");\n"
+			var output = "require(" + JSON.stringify(request) + ");\n"
 				+ loader(source, map);
 
-			return callback(null, source, map);
+			cache[resourcePath] = {source: source, output: output, error: false};
+			return callback(null, output, map);
 		}
 
+		cache[resourcePath] = {error: true};
 		that.emitError('File ' + resourcePath + ' is not reachable from goog-loader entry point.');
 		return callback(null, " ");
 	});
